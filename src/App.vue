a3dd407070632094a9eee9f8b5d3aaa76e18e0ad
张博文 <18211132604@163.com>
2021-03-12 17:55:14 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,65 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      disable-resize-watcher
-      bottom
-      mobile-breakpoint="960"
-      app
-    >
-      <NavBar :navItemsUS="navItemsUS" :navItemsCN="navItemsCN" />
-    </v-navigation-drawer>
-
+    <!-- <v-system-bar  app dark>
+      <v-icon>mdi-message</v-icon>
+      <span>10 unread messages</span>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-minus</v-icon>
+      <v-icon>mdi-checkbox-blank-outline</v-icon>
+      <v-icon>mdi-close</v-icon>
+    </v-system-bar> -->
+    <!-- 头部 总是放在应用顶部，优先级低于 v-system-bar -->
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
-      <router-view />
+    <!-- 侧边导航 可以放置在应用的左边或右边，并且可以配置在 v-app-bar 的旁边或下面 -->
+    <v-navigation-drawer
+      app
+      :value="drawer"
+      disable-resize-watcher
+      bottom
+      mobile-breakpoint="960"
+    >
+      <NavBar :navItemsUS="navItemsUS" :navItemsCN="navItemsCN" />
+    </v-navigation-drawer>
+    <!-- 主视图 当使用 vue-router 时，建议将你的视图放在 v-main 内。 -->
+    <v-main app>
+      <v-container fluid>
+        <router-view />
+      </v-container>
     </v-main>
+
+    <!-- 底部导航 总是放在应用底部，优先级高于 v-footer -->
+    <BottomNav />
+    <!-- footer 总是放在应用底部，优先级低于 v-bottom-navigation -->
+    <!-- <v-footer app>
+      footer
+    </v-footer> -->
   </v-app>
 </template>
 
 <script lang="ts">
 import NavBar from "@/components/navBar";
+import BottomNav from "@/components/bottomNav";
 import { Component, Vue } from "vue-property-decorator";
 import { navItemsUS, navItemsCN } from "@/libs/navData";
 import { INavItem } from "@/components/navBar/index.d";
 @Component({
   components: {
-    NavBar
+    NavBar,
+    BottomNav
   }
 })
 export default class App extends Vue {
-  private drawer = true;
+
+  // private drawer = true;
   private navItemsUS: INavItem[] = navItemsUS;
   private navItemsCN: INavItem[] = navItemsCN;
+  get drawer(){
+    return  this.$vuetify.breakpoint.width > 960
+  }
 }
 </script>
 <style lang="scss" scoped>
---
+++ src/components/bottomNav/bottomNav.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-bottom-navigation
+    v-model="value"
+    :background-color="color"
+    app
+    dark
+    shift
+    :input-value="$vuetify.breakpoint.width<=960"
+  >
+    <v-btn>
+      <span>Video{{ height }}</span>
+
+      <v-icon>mdi-television-play</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Music</span>
+
+      <v-icon>mdi-music-note</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Book</span>
+
+      <v-icon>mdi-book</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Image</span>
+
+      <v-icon>mdi-image</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class BottomNav extends Vue {
+  value = 1;
+  get color() {
+    switch (this.value) {
+      case 0:
+        return "blue-grey";
+      case 1:
+        return "teal";
+      case 2:
+        return "brown";
+      case 3:
+        return "indigo";
+      default:
+        return "blue-grey";
+    }
+  }
+  get height() {
+    console.log("this.$vuetify.breakpoint.name", this.$vuetify.breakpoint,this.$vuetify.breakpoint.width,this.$vuetify.breakpoint.mobile,this.$vuetify.breakpoint.name);
+    switch (this.$vuetify.breakpoint.name) {
+      case "xs":
+        return 220;
+      case "sm":
+        return 400;
+      case "md":
+        return 500;
+      case "lg":
+        return 600;
+      case "xl":
+        return 800;
+    }
+    return 800;
+  }
+}
+</script>
+<style lang="scss" scoped>
+.v-item-group.v-bottom-navigation .v-btn {
+  height: inherit;
+}
+// .v-item-group.v-bottom-navigation--shift .v-btn .v-btn__content > *:not(.v-icon) {
+//   top: calc(100% - 12px);
+// }
+// .v-item-group.v-bottom-navigation{
+//   align-items: center;
+// }
+</style>
