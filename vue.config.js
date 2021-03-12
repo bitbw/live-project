2b6901289e1b76a02e9d0ad98959a57056921509
张博文 <18211132604@163.com>
2021-03-12 16:35:36 +0800

    update router for hash

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   outputDir: "dist",
-  publicPath: process.env.NODE_ENV === "production" ? "/live-project/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/live-project/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "" : "/",
 };
