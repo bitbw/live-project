bb11eeca4731c978f0f4cd76c183e1fef202bd57
张博文 <18211132604@163.com>
2021-04-26 17:37:34 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   outputDir: "dist",
   // git page 需要指定公共路径为仓库名 否则会找不到index.html 中连接的资源
-  publicPath: process.env.NODE_ENV === "production" ? "/live-project/" : "./",
-  // publicPath: "./",
+  // publicPath: process.env.NODE_ENV === "production" ? "/live-project/" : "./",
+  publicPath: "./",
 };
